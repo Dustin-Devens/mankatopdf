8fd24f2a3cd460d0ba61c4e5c70a3652fa3393ea
Dustin Devens <dustin.d.devens@gmail.com>
2021-06-03 16:48:57 -0500

    Initial commit

---
+++ Pages/Tour_Report/tourreport2app.js
@@ -0,0 +1,7 @@
+function openSectionForm() {
+    document.getElementById("sectionForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
